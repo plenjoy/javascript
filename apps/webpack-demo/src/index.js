299bb54b1087f0f70b9a3e5e593f5efd639e6a8e
plenjoy <plenjoy@163.com>
2017-08-23 09:56:19 +0800

    test

--- apps/webpack-demo/src/index.js
+++ apps/webpack-demo/src/index.js
@@ -3,24 +3,30 @@ import './style.css';
 import Icon from './img/icon.png';
 import Icon2 from './img/icon2.png';
 import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
-  var element = document.createElement('div');
+    var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  var myIcon = new Image();
-  myIcon.src=Icon;
-  element.appendChild(myIcon);
-  var myIcon2 = new Image();
-  myIcon2.src=Icon2;
-  element.appendChild(myIcon2);
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    var myIcon2 = new Image();
+    myIcon2.src = Icon2;
+    element.appendChild(myIcon2);
 
-  console.log(Data);
+    console.log(Data);
 
-  
-  return element;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
+    return element;
 }
 
 document.body.appendChild(component());
