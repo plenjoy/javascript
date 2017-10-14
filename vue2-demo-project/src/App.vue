c82e818a5c8ad1b2a8007161fec1ba7fe92042b2
plenjoy <plenjoy@users.noreply.github.com>
2017-10-14 15:28:50 +0800

    del

--- vue2-demo-project/src/App.vue
+++ vue2-demo-project/src/App.vue
@@ -1,28 +1,42 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello-world/>
-  </div>
+    <div id="app">
+        <!-- <hello-world/> -->
+        <loading/>
+    </div>
 </template>
-
 <script>
 import HelloWorld from './components/HelloWorld'
+import Loading from './containers/Loading'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    name: 'app',
+    components: {
+        HelloWorld,
+        Loading
+    }
 }
 </script>
-
 <style>
+html,body{
+    height: 100%;
+    width: 100%;
+    padding:0;
+    margin:0;
+    box-sizing:border-box;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    display: -webkit-flex;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    padding:0;
+    margin:0;
+    box-sizing:border-box;
 }
 </style>
---
+++ vue2-demo-project/src/containers/Loading/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class='loading-div'>
+            <img class='loading-img' src='./Loading.gif' />
+        </div>
+        <div class='loading-txt'>loading...</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Loading',
+    data() {
+        return {}
+    }
+}
+</script>
+<style scoped>
+.loading-div {
+    width: 68px;
+    display: -webkit-flex;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.loading-img {
+    height: 60px;
+    width: 60px;
+}
+
+.loading-txt {
+
+    text-align: center;
+}
+</style>
