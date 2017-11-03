605ec6ec9395421bed37046267cabfc42ed8e27c
plenjoy <plenjoy@users.noreply.github.com>
2017-11-03 10:30:25 +0800

    update

--- vue2-demo-project/src/App.vue
+++ vue2-demo-project/src/App.vue
@@ -24,7 +24,7 @@ export default {
   },
   mounted: function() {
     console.log(this.$root.Store.isShowLoading);
-
+    this.$dispatch('abc');
   }
 }
 
--- vue2-demo-project/src/main.js
+++ vue2-demo-project/src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App'
 
 Vue.config.productionTip = false;
+const dispatchEvent = (type) => {
+  console.log(type);
+}
+Vue.prototype.$dispatch = dispatchEvent;
 const data = {
   Store: Store
 }
